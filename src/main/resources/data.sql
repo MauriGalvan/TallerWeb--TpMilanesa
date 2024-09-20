c8e8149dc9cf2d417bfd0e4136debb77dadb272c
RominaGutierrez <rominamelissagutierrez@gmail.com>
2024-09-19 22:05:54 -0300

    Nuevas recetas

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,4 +6,14 @@ VALUES
 
 ('Tarta jamón y queso', 60.0, 'almuerzo', 'https://i.postimg.cc/XYXRZ1Mq/tarta-jamon-queso.jpg', 'Jamón, Queso, Tapa pascualina, Huevo, Tomate', 'Para comer con tus amigos y familia.', 'Precalienta el horno a 180 grados. Extiende una tapa de pascualina en un molde. Mezcla jamón picado, queso y tomate. Bate un huevo y agrégalo. Vierte sobre la base, cubre con otra tapa si deseas y haz cortes. Hornea 30-35 minutos hasta dorar.'),
 
-('Café cortado con tostadas', 10.0, 'desayuno', 'https://i.postimg.cc/90QVFGGj/cafe-tostada.jpg', 'Café, Leche, Pan lactal, Mermelada', 'Un clásico de las mañanas.', 'Prepara el café a tu gusto y añade un chorrito de leche caliente. Tuesta las rebanadas de pan lactal hasta dorarlas. Unta mermelada en las tostadas. Sirve el café cortado en una taza y acompáñalo con las tostadas.');
+('Café cortado con tostadas', 10.0, 'desayuno', 'https://i.postimg.cc/90QVFGGj/cafe-tostada.jpg', 'Café, Leche, Pan lactal, Mermelada', 'Un clásico de las mañanas.', 'Prepara el café a tu gusto y añade un chorrito de leche caliente. Tuesta las rebanadas de pan lactal hasta dorarlas. Unta mermelada en las tostadas. Sirve el café cortado en una taza y acompáñalo con las tostadas.'),
+
+('Tortas fritas', 20.0, 'merienda', 'https://i.postimg.cc/NMs9s0nP/foto-ilustrativa.jpg', 'Harina, Agua, Sal, Levadura, Azúcar, Aceite', 'Un clásico de la merienda, crujientes y deliciosas.', 'Mezcla la harina, el agua, la sal, el azúcar y la levadura hasta formar una masa. Amasa y deja reposar 10 minutos. Divide en porciones y estira en forma de discos. Fríe en aceite caliente hasta dorar. Escurre y sirve caliente.'),
+
+('Panqueques', 15.0, 'desayuno', 'https://i.postimg.cc/Pq1zy6yv/como-hacer-panqueques-esponjosos-la-receta-facil-y-economica.png', 'Harina, Leche, Huevos, Azúcar, Mantequilla, Sal', 'Suaves y esponjosos, perfectos para el desayuno.', 'Mezcla la harina, leche, huevos, azúcar y sal hasta obtener una masa homogénea. Calienta una sartén con mantequilla y vierte un poco de masa. Cocina hasta que estén dorados y voltea. Sirve con miel o mermelada.'),
+
+('Empanadas de Carne', 60.0, 'almuerzo', 'https://i.postimg.cc/jSPZ3ddj/empandas-de-carne.jpg', 'Masa para empanadas, Carne molida, Cebolla, Huevo, Especias', 'Clásico platillo argentino, perfectas para cualquier ocasión.', 'Cocina la carne con cebolla y especias. Deja enfriar. Rellena las masas con la mezcla y un poco de huevo duro. Cierra las empanadas y hornéalas a 180°C por 20-25 minutos.'),
+
+('Ensalada de Quinoa', 25.0, 'almuerzo', 'https://i.postimg.cc/X7Sbrqhj/ensalada-quinoa-y-verduras.jpg', 'Quinoa, Pimiento, Pepino, Tomate, Cebolla, Limón, Aceite de oliva, Sal', 'Nutritiva y fresca, ideal para el almuerzo.', 'Cocina la quinoa y deja enfriar. Pica las verduras y mezcla en un bol. Aliña con limón, aceite, sal y mezcla bien. Sirve fría, perfecta para días calurosos.');
+
+
